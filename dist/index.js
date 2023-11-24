2ac9162c18b32e1b01bd34b75f33d63c030ba1c3
Arthur <art.nazarian1@gmail.com>
2023-11-24 23:50:40 +0200

    init

---
+++ dist/index.js
@@ -0,0 +1,24 @@
+"use strict";
+// https://en.wikipedia.org/wiki/Xorshift
+const state = [1337];
+const xorshift32 = () => {
+    let x = state[0];
+    x ^= (x << 13) ^ (x >> 17) ^ (x << 5);
+    state[0] = x;
+    return x;
+};
+console.log("xorshift32");
+const NUMBERS_COUNT = 5;
+for (let i = 0; i < NUMBERS_COUNT; i++) {
+    console.log(xorshift32());
+}
+const xorshift64 = () => {
+    let x = state[0];
+    x ^= (x << 13) ^ (x >> 7) ^ (x << 17);
+    return x;
+};
+console.log("\n");
+console.log("xorshift64");
+for (let i = 0; i < NUMBERS_COUNT; i++) {
+    console.log(xorshift32());
+}
