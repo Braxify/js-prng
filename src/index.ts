3d6e87f59134d5b21a1ca5fae67f9016e2f51d93
Arthur <art.nazarian1@gmail.com>
2023-11-25 00:14:27 +0200

    feat: add xorshift128 impl.

--- src/index.ts
+++ src/index.ts
@@ -29,3 +29,25 @@ console.log("xorshift64");
 for (let i = 0; i < NUMBERS_COUNT; i++) {
   console.log(xorshift32());
 }
+
+const stateObj = { a: 1, b: 2, c: 3, d: 4 };
+
+const xorshift128 = (): number => {
+  let t = stateObj.d;
+  const s = stateObj.a;
+
+  stateObj.d = stateObj.c;
+  stateObj.c = stateObj.b;
+  stateObj.b = s;
+
+  t ^= (t << 11) ^ (t >> 8);
+  stateObj.a = t ^ s ^ (s >> 19);
+  return stateObj.a;
+};
+
+console.log("\n");
+console.log("xorshift128");
+
+for (let i = 0; i < NUMBERS_COUNT; i++) {
+  console.log(xorshift128());
+}
